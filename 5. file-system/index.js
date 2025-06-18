2658430d4a9a29260fec78c632164c29d3e24807
Sohag-84 <ihsohag84@gmail.com>
2025-06-18 20:27:56 +0600

    file creating system added in async way

--- 5. file-system/index.js
+++ 5. file-system/index.js
@@ -17,7 +17,22 @@ const readContentFromFile = fs.readFileSync(filePath, "utf8");
 console.log("File content: ", readContentFromFile);
 
 //to add new line on txt file
-fs.appendFileSync(filePath,"\nThis is a new line added to that file")
+fs.appendFileSync(filePath, "\nThis is a new line added to that file");
 console.log("New file content added");
 
+//async way of creating the file
+const asyncPathFile = path.join(dataFolder, "async-example.txt");
+fs.writeFile(asyncPathFile, "Hello, Async node js", (err) => {
+  if (err) throw err;
+  console.log("Async file is created successfully");
 
+  fs.readFile(asyncPathFile, "utf8", (err, data) => {
+    if (err) throw err;
+    console.log("Async file content: ", data);
+  });
+
+  fs.appendFile(asyncPathFile, "\nAnother file is added here", (err) => {
+    if (err) throw err;
+    console.log("New file content added on async file");
+  });
+});
