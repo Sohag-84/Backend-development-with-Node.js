6227071339b9307f4a716f683ad769b93a3727d3
Sohag-84 <ihsohag84@gmail.com>
2025-08-07 12:45:09 +0600

    learnt mongodb basics

---
+++ 5. Mongodb Basics/app.js
@@ -0,0 +1,101 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//injamulhaqsohag19978
+mongoose
+  .connect(
+    process.env.MONGODB_URI
+    // "mongodb+srv://injamulhaqsohag19977:injamulhaqsohag19978@cluster0.htqre1q.mongodb.net/"
+  )
+  .then(() => console.log("Database connected successfully"))
+  .catch((e) => console.log(e));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  isActive: Boolean,
+  tags: [String],
+  createdAt: { type: Date, default: Date.now },
+});
+
+//create user model
+const User = mongoose.model("User", userSchema);
+
+async function runQueryExamples() {
+  try {
+    //create new document
+    const newUser = await User.create({
+      name: "Updated User",
+      email: "abc@gmail.com",
+      age: "130",
+      isActive: true,
+      tags: ["network engineer"],
+    });
+
+    //another way to create new user document
+    // const user = User({
+    //   name: "Raiyan",
+    //   email: "raiyan@gmail.com",
+    //   age: "6",
+    //   isActive: true,
+    //   tags: ["student", "speacker"],
+    // });
+    // await user.save();
+
+    // console.log("Created new user: ", newUser);
+
+    ///get all user
+    // const allUser = await User.find();
+    // console.log(allUser);
+
+    ///specific user
+    // const inActiveUsers = await User.find({ isActive: true });
+    // console.log(inActiveUsers);
+
+    // const findUser = await User.findOne({ name: "Nahid" });
+    // console.log(findUser);
+
+    // const getLastCreatedUserByUserId = await User.findById(newUser._id);
+    // console.log("find user: ", getLastCreatedUserByUserId);
+
+    // const seletedUser = await User.find().select("name email -_id");
+    // console.log(seletedUser);
+
+    ///show 5 user and skip first one user
+    // const limitUser = await User.find().limit(5).skip(1);
+    // console.log(limitUser);
+
+    ///sort user decending order (note: asc-> 1, desc-> -1)
+    // const sortUser = await User.find().sort({ age: -1 });
+    // console.log(sortUser);
+
+    ///count active user
+    // const countDocuments = await User.countDocuments({isActive: false});
+    // console.log(countDocuments);
+
+    // const deleteduser = await User.findByIdAndUpdate(newUser._id);
+    // console.log("Deleted user data: ",deleteduser);
+
+    //update user
+    console.log("Created user: ", newUser);
+    const updatedUser = await User.findByIdAndUpdate(
+      newUser._id,
+      {
+        $set: {
+          age: 80,
+          name: "Mustofa Kamal",
+        },
+        $push: { tags: "updated" },
+      },
+      { new: true }
+    );
+    console.log("Updated user: ", updatedUser);
+  } catch (error) {
+    console.log("Error->", error);
+  } finally {
+    await mongoose.connection.close();
+  }
+}
+
+runQueryExamples();
