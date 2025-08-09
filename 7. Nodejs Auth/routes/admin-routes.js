2215d86a02b1b2a9db15cba269d89e2491bda2e9
Sohag-84 <ihsohag84@gmail.com>
2025-08-09 21:57:30 +0600

    created admin middleware

---
+++ 7. Nodejs Auth/routes/admin-routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const authMiddleware = require("../middleware/auth-middleware");
+const adminMiddleware = require("../middleware/admin-middleware");
+
+const router = express.Router();
+
+router.get("/admin", authMiddleware, adminMiddleware, (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Welcome to admin page",
+  });
+});
+
+module.exports = router;
--- 7. Nodejs Auth/server.js
+++ 7. Nodejs Auth/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const connectToDB = require("./db");
 const authRoutes = require("./routes/auth-route");
 const homeRoutes = require("./routes/home-routes");
+const adminRoutes = require("./routes/admin-routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,7 +15,8 @@ app.use(express.json());
 
 //all routes here
 app.use("/api/auth", authRoutes);
-app.use("/api",homeRoutes);
+app.use("/api", homeRoutes);
+app.use("/api", adminRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
